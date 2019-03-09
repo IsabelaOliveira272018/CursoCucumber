95d9074f0034026ff267e9da895f16bd75c18977
IsabelaOliveira272018 <isa.oliver20@gmail.com>
2019-03-09 19:16:42 -0300

    Gerando mais cenário para validar os testes

--- src/test/java/aulas_cucumber/Primeira_Aula.java
+++ src/test/java/aulas_cucumber/Primeira_Aula.java
@@ -1,9 +1,16 @@
 package aulas_cucumber;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import junit.framework.Assert;
 
+@SuppressWarnings("deprecation")
 public class Primeira_Aula {
 
 	@Given("^que criei o arquivo corretamente$")
@@ -24,25 +31,63 @@ public class Primeira_Aula {
 
 	private int contador = 0;
 
-	@Given("^que o valor do contador e (\\d+)$")
-	public void que_o_valor_do_contador_e(int arg1) throws Throwable {
+	@Given("^que o valor do contador e (\\d+)$") // expressoes regulares regex ... recriando cenarios
+	public void queOValorDoContadorE(int arg1) throws Throwable {
 		contador = arg1;
+
 	}
 
 	@When("^eu incrementar em (\\d+)$")
-	public void eu_incrementar_em(int arg1) throws Throwable {
-
+	public void euIncrementarEm(int arg1) throws Throwable {
 		contador = contador + arg1;
+
 	}
 
 	@Then("^o valor do contador sera (\\d+)$")
-	public void o_valor_do_contador_sera(int arg1) throws Throwable {
-		System.out.println(arg1);
-		System.out.println(contador);
-		System.out.println(arg1 == contador);
-		org.junit.Assert.assertTrue(arg1 == contador);
-		throw new RuntimeException();
-		
+	public void oValorDoContadorSera(int arg1) throws Throwable {
+		Assert.assertEquals(arg1, contador);
+
+	}
+
+	Date entrega = newDate();
+
+	@Given("^que a entrega e dia (\\d+)/(\\d+)/(\\d+)$")
+	public void queAEntregaEDia(int dia, int mes, int ano) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, dia);
+		cal.set(Calendar.MONTH, mes - 1);
+		cal.set(Calendar.YEAR, ano);
+		entrega = cal.getTime();
+	}
+
+	private Date newDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+			//expressao regular
+	@When("^a entrega atrasar em (\\d+) (dia|dias|mes|meses)$")
+	public void aEntregaAtrasarEmDias(int arg1, String tempo) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(entrega);
+		// condicao para que o programa reconheca quando tera que ser dias e mes
+		if (tempo.equals("dias")) {
+			cal.add(Calendar.DAY_OF_MONTH, arg1);
+
+		}
+
+		if (tempo.equals("meses")) {
+			cal.add(Calendar.MONTH, arg1);
+
+		}
+		entrega = cal.getTime();
+	}
+
+	@Then("^a entrega sera efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
+	public void aEntregaSeraEfetuadaEm(String data) throws Throwable {
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		String dataFormatada = format.format(entrega);
+		Assert.assertEquals(data, dataFormatada);
+
 	}
 
 }
--- target/test-classes/aulas_cucumber/aprender_cucumber.feature
+++ target/test-classes/aulas_cucumber/aprender_cucumber.feature
@@ -1,21 +1,33 @@
 Feature: Aprender Cucumber
+  
+  Como aluno
+  Eu quero aprender a utilizar Cucumber
+  Para que eu possa automatizar criterios de aceitacao
 
-	Como aluno
-	Eu quero aprender a utilizar Cucumber
-	Para que eu possa automatizar criterios de aceitacao
+  Scenario: 
+    Given que criei o arquivo corretamente
+    When executar
+    Then a especificacao deve finalizar com sucesso
 
-Scenario:
+ 	Scenario: Deve incrementar contador
+    Given que o valor do contador e 15
+    When eu incrementar em 5
+    Then o valor do contador sera 20
 
-	Given que criei o arquivo corretamente
-	When executar
-	Then a especificacao deve finalizar com sucesso
-	
-	
-Scenario: Deve incrementar contador
-	
-		Given que o valor do contador e 15
-		When eu incrementar em 5
-		Then o valor do contador sera 18
+  Scenario: Deve incrementar contador
+    Given que o valor do contador e 123
+    When eu incrementar em 35
+    Then o valor do contador sera 158
 
-
-
+    Scenario: Deve calcular atraso no prazo de entrega
+    Given que a entrega e dia 05/04/2018
+    When a entrega atrasar em 2 dias
+    Then a entrega sera efetuada em 07/04/2018
+    
+    Scenario: Deve calcular atraso no prazo de entrega da China
+    Given que a entrega e dia 05/04/2018
+    When a entrega atrasar em 2 meses 
+    Then a entrega sera efetuada em 05/06/2018
+    
+    
+    
