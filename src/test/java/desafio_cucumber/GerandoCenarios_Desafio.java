6cf0c32ec7725e83d73c2ffdf2880b4d5f3a2d92
IsabelaOliveira272018 <isa.oliver20@gmail.com>
2019-03-09 19:50:10 -0300

    Inicializando os métodos

--- src/test/java/desafio_cucumber/GerandoCenarios_Desafio.java
+++ src/test/java/desafio_cucumber/GerandoCenarios_Desafio.java
@@ -1,7 +1,39 @@
 package desafio_cucumber;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class GerandoCenarios_Desafio {
 	
-	
+	@Given("^que o ticket ? AF(\\d+)$")
+	public void queOTicketAF(int arg1) throws Throwable {
+	    
+	}
+
+	@Given("^que o valor da passagem ? R\\$ (\\d+),(\\d+)$")
+	public void queOValorDaPassagemR$(int arg1, int arg2) throws Throwable {
+	   
+	}
+
+	@Given("^que o nome do passageiro ? \"([^\"]*)\"$")
+	public void queONomeDoPassageiro(String arg1) throws Throwable {
+	    
+	}
+
+	@Given("^que o telefone do passageiro ? (\\d+)-(\\d+)$")
+	public void queOTelefoneDoPassageiro(int arg1, int arg2) throws Throwable {
+	   
+	}
+
+	@When("^criar os steps$")
+	public void criarOsSteps() throws Throwable {
+	    
+	}
+
+	@Then("^o teste vai funcionar$")
+	public void oTesteVaiFuncionar() throws Throwable {
+	    
+	}
 
 }
