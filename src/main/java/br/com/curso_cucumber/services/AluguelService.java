5bf24e2051c69d6497ee135474537b7047cc5632
IsabelaOliveira272018 <isa.oliver20@gmail.com>
2019-04-20 00:26:44 -0300

    Refatorando o código

--- src/main/java/br/com/curso_cucumber/services/AluguelService.java
+++ src/main/java/br/com/curso_cucumber/services/AluguelService.java
@@ -1,24 +1,22 @@
 package br.com.curso_cucumber.services;
 
-import java.util.Calendar;
-
 import br.com.curso_cucumber.entidades.Filme;
 import br.com.curso_cucumber.entidades.NotaAluguel;
+import br.com.curso_cucumber.utils.DateUtils;
 
 public class AluguelService {
 
 	public NotaAluguel alugar(Filme filme) {
-		if(filme.getEstoque() == 0) 
+		if (filme.getEstoque() == 0)
 			throw new RuntimeException("Filme sem estoque");
+
 		NotaAluguel nota = new NotaAluguel();
 		nota.setPreco(filme.getAluguel());
-		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.DAY_OF_MONTH, 1);
-		nota.setDataEntrega(cal.getTime());
-		nota.setDataEntrega(cal.getTime());
+		nota.setDataEntrega(DateUtils.obterDataDiferencaDias(1));
+
 		filme.setEstoque(filme.getEstoque() - 1);
 		return nota;
-		
-	} 
+
+	}
 
 }
