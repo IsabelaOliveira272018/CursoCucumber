eea7685f7d7f62920f3cb6d083aa461dfbd7e737
IsabelaOliveira272018 <isa.oliver20@gmail.com>
2019-04-20 00:46:31 -0300

    Gerando condições

--- src/main/java/br/com/curso_cucumber/services/AluguelService.java
+++ src/main/java/br/com/curso_cucumber/services/AluguelService.java
@@ -11,7 +11,14 @@ public class AluguelService {
 			throw new RuntimeException("Filme sem estoque");
 
 		NotaAluguel nota = new NotaAluguel();
-		nota.setPreco(filme.getAluguel());
+		if ("extendido".equals(tipoAluguel)) {
+			nota.setPreco(filme.getAluguel() * 2);
+		}
+		
+		else {
+			nota.setPreco(filme.getAluguel());
+
+		}
 		nota.setDataEntrega(DateUtils.obterDataDiferencaDias(1));
 
 		filme.setEstoque(filme.getEstoque() - 1);
